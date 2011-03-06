ff5764d2b68592d95d130c3368a0fc2bc6b393f3
Daniel Hartnett <thelinked@gmail.com>
2011-03-06 23:13:53 +0000

    Added required dll's

---
+++ bin/exe/roots.lua
@@ -0,0 +1,33 @@
+Roots =
+{
+    Windows =
+    {
+        textures = "../Resources/Core/Images/",
+        shaders = "../Resources/Core/Shaders/",
+        meshes = "../Resources/Core/Meshes/",
+        fonts = "../Resources/Core/Fonts/",
+        music = "../Resources/Core/Music/",
+        soundeffects = "../Resources/Core/SoundEffect/",
+        resources = "../Resources/",
+        settings = "../Settings/",
+        ships = "../Settings/Ships/",
+        bullets = "../Settings/Bullets/",
+        particles = "../Settings/Particles/",
+        lua = "../Lua/"
+    },
+    Mac =
+    {
+        textures = "../Resources/Core/Images/",
+        shaders = "../Resources/Core/Shaders/",
+		meshes = "../Resources/Core/Meshes/",
+        fonts = "../Resources/Core/Fonts/",
+        music = "../Resources/Core/Music/",
+        soundeffects = "../Resources/Core/SoundEffect/",
+		resources = "../Resources/",
+        settings = "../Settings/",
+		ships = "../Settings/Ships/",
+        bullets = "../Settings/Bullets/",
+        particles = "../Settings/Particles/",
+        lua = "../Lua/"
+    }
+}
